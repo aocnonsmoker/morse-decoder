a30ef85a85e96d572c9b960beff4b2fe943c9d6e
Zhambyl Kairken <z.kairken@csi.kz>
2021-08-19 21:15:47 +0600

    morse decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = [];
+    let result = '';
+    for (let i = 0, charsLength = String(expr).length; i < charsLength; i += 10) {
+        arr.push(String(expr).substring(i, i + 10));
+    }
+    arr.forEach((item) => {
+        const substr = [];
+        let morse = '';
+        if (item !== '**********') {
+            for (let i = 0, charsLength = item.length; i < charsLength; i += 2) {
+                substr.push(String(item).substring(i, i + 2));
+            }
+            for (let i = 0; i < substr.length; i++) {
+                if (substr[i] == '10') {
+                    morse += '.'
+                }
+                if (substr[i] == '11') {
+                    morse += '-'
+                }
+            }
+            if (MORSE_TABLE[morse] !== undefined) {
+                result += MORSE_TABLE[morse]
+            }
+        } else {
+            result += ' '
+        }
+    });
+    return result;
 }
 
 module.exports = {
